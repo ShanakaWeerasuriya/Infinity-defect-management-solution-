cb987b4f988d226f36c9d69346ea1d9f404968f2
terrance shanaka <shanakadesilva46@gmail.com>
2019-01-30 14:00:29 +0530

    Created Layers

--- InfinityDefectManagement/InfinityDefectManagement/Program.cs
+++ InfinityDefectManagement/InfinityDefectManagement/Program.cs
@@ -17,7 +17,7 @@ namespace InfinityDefectManagement
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            //Appliication.
+            
         }
     }
 }
